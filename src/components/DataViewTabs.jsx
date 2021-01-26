6dbede247dd00f3c93f786bd6fe885b209a2968f
Usman Haider <uhaider.bese15seecs@seecs.edu.pk>
2021-01-27 00:53:39 +0500

    corrected table, chart styles

--- src/components/DataViewTabs.jsx
+++ src/components/DataViewTabs.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
@@ -7,34 +7,20 @@ import { TableChart, BarChart } from "@material-ui/icons";
 import TableView from "./common/TableView";
 import ChartView from "./common/ChartView";
 import TabPanel from "./common/TabPanel";
+import SwipeableViews from "react-swipeable-views";
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    width: "100%",
-    backgroundColor: theme.palette.background.paper,
-  },
-  appBar: {
-    backgroundColor: "white",
-    boxShadow: "none",
-    "& .MuiTabs-root": {
-      minHeight: "0px",
-    },
-  },
+  root: {},
   tab: {
-    padding: "0px !important",
-    minHeight: "0px",
+    minHeight: 0,
     "& .MuiTab-wrapper": {
-      padding: "10px 10px 10px 10px",
-      backgroundColor: " #f3f1f1",
       flexDirection: "row !important",
     },
   },
-  tabPanel: {
-    boxShadow: "2px 2px 6px 0px #888888",
-    borderRadius: "0px 10px 10px 10px",
-    "& .MuiBox-root": {
-      padding: 8,
-    },
+  content: {
+    boxShadow: "1px 1px 5px gray;",
+    paddingTop: 8,
+    borderRadius: 10,
   },
 }));
 
@@ -45,38 +31,51 @@ export default function DataViewTabs({
   secondPlayer,
 }) {
   const classes = useStyles();
+  const theme = useTheme();
   const [value, setValue] = React.useState(0);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
+  const handleChangeIndex = (index) => {
+    setValue(index);
+  };
+
   return (
     <div className={classes.root}>
-      <AppBar position="static" color="default" className={classes.appBar}>
+      <AppBar position="static" color="default">
         <Tabs
           value={value}
           onChange={handleChange}
           scrollButtons="on"
           indicatorColor="primary"
           textColor="primary"
+          variant="fullWidth"
         >
           <Tab label="Table" icon={<TableChart />} className={classes.tab} />
           <Tab label="Chart" icon={<BarChart />} className={classes.tab} />
         </Tabs>
       </AppBar>
-      <TabPanel value={value} index={0} className={classes.tabPanel}>
-        <TableView data={data} />
-      </TabPanel>
-      <TabPanel value={value} index={1} className={classes.tabPanel}>
-        <ChartView
-          chartData={{
-            ...chartData,
-            first_player_name: firstPlayer.player_name,
-            second_player_name: secondPlayer.player_name,
-          }}
-        />
-      </TabPanel>
+      <SwipeableViews
+        axis={theme.direction === "rtl" ? "x-reverse" : "x"}
+        index={value}
+        onChangeIndex={handleChangeIndex}
+        className={classes.content}
+      >
+        <TabPanel value={value} index={0}>
+          <TableView data={data} />
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          <ChartView
+            chartData={{
+              ...chartData,
+              first_player_name: firstPlayer.player_name,
+              second_player_name: secondPlayer.player_name,
+            }}
+          />
+        </TabPanel>
+      </SwipeableViews>
     </div>
   );
 }
