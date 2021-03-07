6455cd9d44f3fe59c9f9ed3cd7ef3b524524af53
Usman Haider <uhaider.bese15seecs@seecs.edu.pk>
2021-03-07 17:12:21 +0500

    added icc player batting and bowling comparisons

--- src/components/IccCareerComparisons.jsx
+++ src/components/IccCareerComparisons.jsx
@@ -1,104 +1,19 @@
 import React from "react";
-import SwipeableViews from "react-swipeable-views";
-import { makeStyles, useTheme } from "@material-ui/core/styles";
-import AppBar from "@material-ui/core/AppBar";
-import Tabs from "@material-ui/core/Tabs";
-import Tab from "@material-ui/core/Tab";
-import IccBattingFormatAveragesComparison from "./IccBattingFormatAveragesComparison";
-import TabPanel from "./common/TabPanel";
-import IccCareerAveragesComparison from "./IccCareerAveragesComparison";
-import { battingAverageOptions, bowingAverageOptions } from "../data/data";
-
-function a11yProps(index) {
-  return {
-    id: `full-width-tab-${index}`,
-    "aria-controls": `full-width-tabpanel-${index}`,
-  };
-}
-
-const useStyles = makeStyles((theme) => ({
-  tabPanel: {
-    "& .MuiBox-root": {
-      padding: 8,
-    },
-  },
-  root: {
-    backgroundColor: theme.palette.background.paper,
-    width: "100%",
-  },
-}));
+import Grid from "@material-ui/core/Grid";
+import IccPlayersBattingAveragesComparison from "./comparisons/IccPlayersBattingAveragesComparison";
+import IccPlayersBowlingAveragesComparison from "./comparisons/IccPlayersBowlingAveragesComparison";
 
 export default function IccCareerComparisons({ firstPlayer, secondPlayer }) {
-  const classes = useStyles();
-  const theme = useTheme();
-  const [value, setValue] = React.useState(0);
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-
-  const handleChangeIndex = (index) => {
-    setValue(index);
-  };
-
   return (
-    <div className={classes.root}>
-      <AppBar position="static" color="default">
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          indicatorColor="primary"
-          textColor="primary"
-          variant="fullWidth"
-          aria-label="full width tabs example"
-        >
-          <Tab label="Batting" {...a11yProps(0)} />
-          <Tab label="Bowling" {...a11yProps(1)} />
-        </Tabs>
-      </AppBar>
-      <SwipeableViews
-        axis={theme.direction === "rtl" ? "x-reverse" : "x"}
-        index={value}
-        onChangeIndex={handleChangeIndex}
-      >
-        <TabPanel
-          value={value}
-          index={0}
-          dir={theme.direction}
-          className={classes.tabPanel}
-        >
-          <IccCareerAveragesComparison
-            firstPlayer={firstPlayer}
-            secondPlayer={secondPlayer}
-            career="batting"
-          />
-
-          <IccBattingFormatAveragesComparison
-            firstPlayer={firstPlayer}
-            secondPlayer={secondPlayer}
-            career="batting"
-            options={battingAverageOptions}
-          />
-        </TabPanel>
-        <TabPanel
-          value={value}
-          index={1}
-          dir={theme.direction}
-          className={classes.tabPanel}
-        >
-          <IccCareerAveragesComparison
-            firstPlayer={firstPlayer}
-            secondPlayer={secondPlayer}
-            career="bowling"
-          />
-          <IccBattingFormatAveragesComparison
-            firstPlayer={firstPlayer}
-            secondPlayer={secondPlayer}
-            career="bowling"
-            options={bowingAverageOptions}
-          />
-        </TabPanel>
-      </SwipeableViews>
-    </div>
+    <Grid container>
+      <IccPlayersBattingAveragesComparison
+        firstPlayer={firstPlayer}
+        secondPlayer={secondPlayer}
+      />
+      <IccPlayersBowlingAveragesComparison
+        firstPlayer={firstPlayer}
+        secondPlayer={secondPlayer}
+      />
+    </Grid>
   );
 }
---
+++ src/components/comparisons/IccPlayersBattingAveragesComparison.jsx
@@ -0,0 +1,147 @@
+import React, { useEffect } from "react";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import Select from "@material-ui/core/Select";
+import { makeStyles } from "@material-ui/core/styles";
+import axios from "axios";
+import TableView from "../common/TableView";
+import Grid from "@material-ui/core/Grid";
+import { icc_teams_lookup } from "../../data/data";
+
+const useStyles = makeStyles((theme) => ({
+  root: { padding: 0, marginBottom: 10 },
+
+  button: {
+    display: "block",
+  },
+  formControl: {
+    minWidth: 100,
+  },
+}));
+
+const IccPlayersBattingAveragesComparison = ({ firstPlayer, secondPlayer }) => {
+  const classes = useStyles();
+  const [chartData, setChartData] = React.useState({
+    first_player: {},
+    second_player: {},
+  });
+
+  const [formatType, setFormatType] = React.useState("All Formats");
+  const [formatMenuOpen, setFormatMenuOpen] = React.useState(false);
+
+  const handleFormatChange = (event) => {
+    setFormatType(event.target.value);
+  };
+
+  const handleFormatMenuClose = () => {
+    setFormatMenuOpen(false);
+  };
+
+  const handleFormatMenuOpen = () => {
+    setFormatMenuOpen(true);
+  };
+
+  const [oppositionOption, setOppositionOption] = React.useState("all_teams");
+  const [oppositionMenuOpen, setOppositionMenuOpen] = React.useState(false);
+
+  const handleOppositionChange = (event) => {
+    setOppositionOption(event.target.value);
+  };
+
+  const handleOppositionMenuClose = () => {
+    setOppositionMenuOpen(false);
+  };
+
+  const handleOppositionMenuOpen = () => {
+    setOppositionMenuOpen(true);
+  };
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3001/icc_comparison/career_averages_comparison`, {
+        params: {
+          first_player_id: firstPlayer.player_id,
+          second_player_id: secondPlayer.player_id,
+          format_type: formatType,
+          opposition_team: oppositionOption,
+          type: "batting",
+        },
+      })
+      .then(function ({ data }) {
+        const { first_player, second_player } = data;
+
+        setChartData({ first_player, second_player });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, [
+    formatType,
+    oppositionOption,
+    firstPlayer.player_id,
+    secondPlayer.player_id,
+  ]);
+
+  return (
+    <Grid container>
+      <div>
+        <FormControl variant="outlined" className={classes.formControl}>
+          <InputLabel id="demo-controlled-open-select-label">Format</InputLabel>
+          <Select
+            labelId="demo-controlled-open-select-label"
+            id="demo-controlled-open-select"
+            open={formatMenuOpen}
+            onClose={handleFormatMenuClose}
+            onOpen={handleFormatMenuOpen}
+            value={formatType}
+            onChange={handleFormatChange}
+            label="Format"
+            className={classes.root}
+          >
+            {["All Formats", "T20Is", "Tests", "ODIs"].map((format) => (
+              <MenuItem value={format}>{format}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+
+        <FormControl variant="outlined" className={classes.formControl}>
+          <InputLabel id="demo-controlled-open-select-label">
+            Opposition
+          </InputLabel>
+          <Select
+            labelId="demo-controlled-open-select-label"
+            id="demo-controlled-open-select"
+            open={oppositionMenuOpen}
+            onClose={handleOppositionMenuClose}
+            onOpen={handleOppositionMenuOpen}
+            value={oppositionOption}
+            onChange={handleOppositionChange}
+            label="Opposition"
+            className={classes.root}
+          >
+            {Object.entries(icc_teams_lookup).map(([value, label]) => (
+              <MenuItem value={value}>{label}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+      <div
+        style={{
+          boxShadow: "2px 2px 6px 0px #888888",
+          borderRadius: "10px 10px 10px 10px",
+          padding: 8,
+          marginBottom: 20,
+          width: "100%",
+        }}
+      >
+        <TableView
+          data={chartData}
+          excludedKeys={["player_id", "format_type", "opposition_team"]}
+        />
+      </div>
+    </Grid>
+  );
+};
+
+export default IccPlayersBattingAveragesComparison;
