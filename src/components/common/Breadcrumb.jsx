95fc10933555331123f561532c56bfbee4985d98
Usman Haider <uhaider.bese15seecs@seecs.edu.pk>
2021-04-03 17:21:33 +0500

    formatted breadcrumb

--- src/components/common/Breadcrumb.jsx
+++ src/components/common/Breadcrumb.jsx
@@ -3,14 +3,16 @@ import Breadcrumbs from "@material-ui/core/Breadcrumbs";
 import { Link } from "react-router-dom";
 import { withRouter } from "react-router";
 import humanify from "../../Utils/humanify";
+import getFormattedBreadcrumb from "../../Utils/getFormattedBreadcrumb";
 
 const Breadcrumb = ({ history }) => {
+  console.log("history.location.pathname: ", history.location.pathname);
   return (
     <Breadcrumbs aria-label="breadcrumb" style={{ marginBottom: 20 }}>
       <Link to="/">Home</Link>
       {history.location.pathname != "/" ? (
         <Link to={history.location.pathname}>
-          {humanify(history.location.pathname.match("[^/]+$")[0])}
+          {getFormattedBreadcrumb(history.location.pathname)}
         </Link>
       ) : null}
     </Breadcrumbs>
