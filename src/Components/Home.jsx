9e2564a4bcfe9e70bf017e36592a5c3f99944f4a
Usman Haider <uhaider.bese15seecs@seecs.edu.pk>
2021-04-06 16:03:19 +0500

    refactor

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,13 +1,24 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import { Link } from "react-router-dom";
 import Breadcrumb from "Components/Common/Breadcrumb";
+import HomeCard from "Components/Common/HomeCard";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+    textAlign: "center",
+  },
+  cardActionArea: {
+    height: 100,
+  },
+  cardContent: {
+    padding: 0,
+  },
+  cardActions: {
+    display: "flex",
+    justifyContent: "center",
   },
   paper: {
     padding: theme.spacing(2),
@@ -24,45 +35,72 @@ const Home = () => {
   return (
     <div className={classes.root}>
       <Breadcrumb />
-      <Grid container spacing={1}>
-        <Grid item xs={12}>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6}>
           <Link to="/profiles/icc_profile" style={{ textDecoration: "none" }}>
-            <Paper className={classes.paper}>ICC Player Profile</Paper>
+            <HomeCard
+              heading="ICC Player Profile"
+              paragraph="Get insight into batting and bowling averages of your
+                    favorite player"
+              btnLabel="See Profile"
+            />
           </Link>
         </Grid>
 
-        <Grid item xs={12}>
+        <Grid item xs={12} sm={6}>
           <Link
             to="/comparisons/icc_comparison"
             style={{ textDecoration: "none" }}
           >
-            <Paper className={classes.paper}>ICC Player comparisons</Paper>
+            <HomeCard
+              heading="ICC Player Comparison"
+              paragraph="Compare batting and bowling careers of your favorite players"
+              btnLabel="See Comparison"
+            />
           </Link>
         </Grid>
-        <Grid item xs={12}>
+        <Grid item xs={12} sm={6}>
           <Link to="/profiles/psl_profile" style={{ textDecoration: "none" }}>
-            <Paper className={classes.paper}>PSL Player Profile</Paper>
+            <HomeCard
+              heading="PSL Player Profile"
+              paragraph="Get insight into batting and bowling averages of your
+                    favorite player"
+              btnLabel="See Profile"
+            />
           </Link>
         </Grid>
-        <Grid item xs={12}>
+        <Grid item xs={12} sm={6}>
           <Link
             to="/comparisons/psl_comparison"
             style={{ textDecoration: "none" }}
           >
-            <Paper className={classes.paper}>PSL Player comparisons</Paper>
+            <HomeCard
+              heading="PSL Player Comparison"
+              paragraph="Compare batting and bowling careers of your favorite players"
+              btnLabel="See Comparison"
+            />
           </Link>
         </Grid>
-        <Grid item xs={12}>
+        <Grid item xs={12} sm={6}>
           <Link to="/profiles/ipl_profile" style={{ textDecoration: "none" }}>
-            <Paper className={classes.paper}>IPL Player Profile</Paper>
+            <HomeCard
+              heading="IPL Player Profile"
+              paragraph="Get insight into batting and bowling averages of your
+                    favorite player"
+              btnLabel="See Profile"
+            />
           </Link>
         </Grid>
-        <Grid item xs={12}>
+        <Grid item xs={12} sm={6}>
           <Link
             to="/comparisons/ipl_comparison"
             style={{ textDecoration: "none" }}
           >
-            <Paper className={classes.paper}>IPL Player comparisons</Paper>
+            <HomeCard
+              heading="IPL Player Comparison"
+              paragraph="Compare batting and bowling careers of your favorite players"
+              btnLabel="See Comparison"
+            />
           </Link>
         </Grid>
       </Grid>
