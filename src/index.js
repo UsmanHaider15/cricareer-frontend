748c453e9379ddd07c9ec6c0fa7be69de18c376e
Usman Haider <uhaider.bese15seecs@seecs.edu.pk>
2021-07-11 19:04:15 +0500

    removed gh-pages workflow

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import Footer from "Components/Common/Footer";
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter>
       <App />
       <Footer />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
