374503912f9327a9fad520bcec92bb861d280d81
Usman Haider <uhaider.bese15seecs@seecs.edu.pk>
2021-07-11 15:59:45 +0500

    removed docs addes gh-pages deployemnt

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import Footer from "Components/Common/Footer";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
       <Footer />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
